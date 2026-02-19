61ee62b4ccce9e5a8e8e82b71326b82cf753d1e6
Kushal <duwadikushal99@gmail.com>
2026-02-19 08:57:28 +0545

    Add marquee on initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,14 +3,25 @@ import { Download, Sun, Moon } from "lucide-react";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <Download size={34} color="red" /> FetchVid
+    <div>
+      {/* Top scrolling banner */}
+      <div className="top-banner">
+        <span>🚧 Still in Progress 🚧</span>
       </div>
-      <button className="theme-btn" onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? <Sun size={18} /> : <Moon size={18} />}
-      </button>
-    </nav>
+
+      <nav className="navbar">
+        <div className="logo">
+          <Download size={34} color="red" /> FetchVid
+        </div>
+
+        <button
+          className="theme-btn"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {darkMode ? <Sun size={18} /> : <Moon size={18} />}
+        </button>
+      </nav>
+    </div>
   );
 };
 
